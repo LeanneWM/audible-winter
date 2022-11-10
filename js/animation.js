19a38ffa30500a1b39e0a43b830ce3c4a5f12d14
DanShepherd95 <daniel.shepherd@wmglobal.com>
2022-11-10 12:50:23 +0000

    animation change

--- js/animation.js
+++ js/animation.js
@@ -4,12 +4,12 @@ function startAnimation() {
     //Audible Animation
     var tl = gsap.timeline({repeat: 1, repeatDelay: 1});
     tl.to("#s1_logo", {duration: 0.3, x: 0});
-    tl.fromTo("#s1_text1", {x: -200}, {ease: "back.out(1.2)", duration: 0.3, x: 0});
-    tl.fromTo("#s1_text2", {x: -200}, {ease: "back.out(1.2)", duration: 0.3, x: 0});
-    tl.fromTo("#s1_text3", {x: -200}, {ease: "back.out(1.2)", duration: 0.3, x: 0});
-    tl.fromTo("#s1_text4", {x: -200}, {ease: "back.out(1.2)", duration: 0.3, x: 0});
+    tl.fromTo("#s1_text1", {x: -200}, {ease: "back.out(1.2)", duration: 0.5, x: 0});
+    tl.fromTo("#s1_text2", {x: -200}, {ease: "back.out(1.2)", duration: 0.5, x: 0},"<0.3");
+    tl.fromTo("#s1_text3", {x: -200}, {ease: "back.out(1.2)", duration: 0.5, x: 0},"<0.3");
+    tl.fromTo("#s1_text4", {x: -200}, {ease: "back.out(1.2)", duration: 0.5, x: 0},"<0.3");
     tl.fromTo("#s1_img1", {x: 400}, {ease: "back.out(1.2)", duration: 0.6, x: 0});
-    tl.fromTo("#s1_img2", {x: 400}, {ease: "back.out(1.2)", duration: 0.6, x: 0});
+    tl.fromTo("#s1_img2", {x: 400}, {ease: "back.out(1.2)", duration: 0.6, x: 0}, "<0.3");
     tl.fromTo("#s1_cta", {y: 400}, {duration: 0.3, y: 0});
 
     // //Variable Declarations
